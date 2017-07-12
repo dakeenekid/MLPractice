cfdb9a27b0895afa3165939812b6a7df38223fac
dakeenekid <dbkeene.tsyc@gmail.com>
2017-07-12 17:47:47 -0400

    Gender classifier and linear regression test added.

--- MLPractice/basic_classifier.py
+++ MLPractice/basic_classifier.py
@@ -1,17 +1,21 @@
+#Used to determine men from women based on height and shoe size
+
 from sklearn import tree
 
-import numpy as np
-
-
+#height and shoe size
 X = [[65,9],[67,7],[70,11],[62,6],[60,7],[72,13],[66,10],[67,7.5]]
 
 Y=["male","female","male","female","female","male","male","female"]
 
+#creating a decision tree
 clf = tree.DecisionTreeClassifier()
 
+#fitting the data to the tree
 clf.fit(X, Y)
 
+#predicting the gender based on a prediction
 prediction = clf.predict([62,7])
 
+#print the predicted gender
 print(prediction)
 
