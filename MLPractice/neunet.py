a25865ee8b9d90665c1a62dd787c2d21f140982c
Davis <dbkeene.tsyc@gmail.com>
2017-07-29 16:33:40 -0400

    Updated Neural Net

--- MLPractice/neunet.py
+++ MLPractice/neunet.py
@@ -1,54 +1,77 @@
+#Basic neural net that I am making from scratch. This is a step up from base_classifier, for I am using neural nets. This net is going to predict the likelyhood of a person being either male or female, based on shoe size and height.
 import numpy as np
+import tensorflow
+import pandas
 
-#Input array
-X=np.array([[1,0,1,0],[1,0,1,1],[0,1,0,1]])
+#Creating datasets
 
-#Output
-y=np.array([[1],[1],[0]])
+#Each X entry is a person's shoe size followed by their height
+#Each Y entry is a person's odds of being a male. 100% indicates a man, and 0% indicates a woman.
 
-#Sigmoid Function
-def sigmoid (x):
-    return 1/(1 + np.exp(-x))
+X = np.array(([10,65],[9,63],[70,12],[7,60],[7,63]) ,dtype=float)
+Y = np.array(([1],[0],[1],[1],[0],) ,dtype=float)
 
-#Derivative of Sigmoid Function
-def derivatives_sigmoid(x):
-    return x * (1 - x)
+#Scaling data
 
-#Variable initialization
-epoch=10000000 #Setting training iterations
-lr=0.1 #Setting learning rate
+X = X/np.amax(X, axis=0)
+Y = Y/100
 
-inputlayer_neurons = X.shape[1] #number of features in data set
-hiddenlayer_neurons = 4 #number of hidden layers neurons
-output_neurons = 1 #number of neurons at output layer
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
 
-#weight and bias initialization
-wh=np.random.uniform(size=(inputlayer_neurons,hiddenlayer_neurons))
-bh=np.random.uniform(size=(1,hiddenlayer_neurons))
-wout=np.random.uniform(size=(hiddenlayer_neurons,output_neurons))
-bout=np.random.uniform(size=(1,output_neurons))
 
-for i in range(epoch):
+class neuralNet(object):
+    def __init__(self):
+        # Define Hyperparameters
+        self.inputLayerSize = 2
+        self.outputLayerSize = 1
+        self.hiddenLayerSize = 3
 
-#Forward Propogation
-    hidden_layer_input1=np.dot(X,wh)
-    hidden_layer_input=hidden_layer_input1 + bh
-    hiddenlayer_activations = sigmoid(hidden_layer_input)
-    output_layer_input1=np.dot(hiddenlayer_activations,wout)
-    output_layer_input= output_layer_input1+ bout
-    output = sigmoid(output_layer_input)
+        # Weights (parameters)
+        self.W1 = np.random.randn(self.inputLayerSize, self.hiddenLayerSize)
+        self.W2 = np.random.randn(self.hiddenLayerSize, self.outputLayerSize)
 
-#Backpropagation
-    E = y-output
-    slope_output_layer = derivatives_sigmoid(output)
-    slope_hidden_layer = derivatives_sigmoid(hiddenlayer_activations)
-    d_output = E * slope_output_layer
-    Error_at_hidden_layer = d_output.dot(wout.T)
-    d_hiddenlayer = Error_at_hidden_layer * slope_hidden_layer
-    wout += hiddenlayer_activations.T.dot(d_output) *lr
-    bout += np.sum(d_output, axis=0,keepdims=True) *lr
-    wh += X.T.dot(d_hiddenlayer) *lr
-    bh += np.sum(d_hiddenlayer, axis=0,keepdims=True) *lr
+    def forward(self, X):
+        # Propogate inputs though network
+        self.z2 = np.dot(X, self.W1)
+        self.a2 = self.sigmoid(self.z2)
+        self.z3 = np.dot(self.a2, self.W2)
+        yHat = self.sigmoid(self.z3)
+        return yHat
 
-print(output)
+    def sigmoid(self, z):
+        # Apply sigmoid activation function to scalar, vector, or matrix
+        return 1 / (1 + np.exp(-z))
+
+    def sigmoidPrime(self, z):
+        # Gradient of sigmoid
+        return np.exp(-z) / ((1 + np.exp(-z)) ** 2)
+
+    def costFunction(self, X, y):
+        # Compute cost for given X,y, use weights already stored in class.
+        self.yHat = self.forward(X)
+        J = 0.5 * sum((y - self.yHat) ** 2)
+        return J
+
+    def costFunctionPrime(self, X, y):
+        # Compute derivative with respect to W and W2 for a given X and y:
+        self.yHat = self.forward(X)
+
+        delta3 = np.multiply(-(y - self.yHat), self.sigmoidPrime(self.z3))
+        dJdW2 = np.dot(self.a2.T, delta3)
+
+        delta2 = np.dot(delta3, self.W2.T) * self.sigmoidPrime(self.z2)
+        dJdW1 = np.dot(X.T, delta2)
+
+        return dJdW1, dJdW2
+
+
+
+
+net = neuralNet()
+prediction = net.forward(X)
+cost1 = net.costFunction(X,Y)
+dJdW1, dJdW2 = net.costFunctionPrime(X,Y)
+print(dJdW2)
+
 
