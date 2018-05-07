a969234820ef0b20fd8a074371c88fb216f0076b
Davis <dbkeene.tsyc@gmail.com>
2018-05-06 20:15:16 -0400

    fixed stuff

--- MLPractice/Probability/plotProb.py
+++ MLPractice/Probability/plotProb.py
@@ -1,22 +1,20 @@
 from matplotlib import pyplot as plt
 from scipy.special import comb
 import numpy as np
+# Program inspired by AB Pre-Calc warm-up problem below
+
 # Mr. Cole has a 90% chance of making a free throw. What is the probability that he makes...
 
 # All 10 FT's, At least 8, etc.
 
-# Global Veriable that keeps track of the sum of probabilities that we want
-sum_prob = 0
-
-# Array that stores all of our probabilities. It's empty right now.
-values = np.array([])
-
 # Define our function. This will plot the probabilities, and therfore we should call it plot_prob
-def plot_prob(n,prob,chance):
+def plot_prob(n,prob,r):
+    # Global Veriable that keeps track of the sum of probabilities that we want
+    sum_prob = 0
+    values = np.array([]) # Array that stores all of our probabilities. It's empty upon initialization.
+    ax = plt.subplot(111) # Sub-plot for text (not important)
     # For loop that will run n+1 times, to account for 0
     for i in range(0,n+1):
-        global sum_prob
-        global values
         # Declare our co-efficient, which we can get using the binomial theorem
         # The comb function returns n choose i
         coef = comb(n,i)
@@ -27,9 +25,8 @@ def plot_prob(n,prob,chance):
         # This number is the chance that Mr. Cole will make i shots
         # i gets updated after each iteration (i=0, then i=1, and so on...)
         y_ = coef*(prob**(n-i))*(invprob**i)
-        print("Probability of making",n-i,"shots:",y_)
         # Add our probabilities if they're in bounds
-        if(n-chance >= i):
+        if(n-r >= i):
             sum_prob = sum_prob + y_
         # Just adds a small annotation to each data point, signifying each probability on the graph
         plt.annotate(round(y_,3), xy=(n-i, y_), xytext=(n-i, y_ + .01))
@@ -41,10 +38,17 @@ def plot_prob(n,prob,chance):
     x = np.arange(0, values.size)
     # We need to reverse the order of the array, in order for the plot and the points to line up
     plt.plot(x, values[::-1])
-    text = "P of making at least {} shots: {}{}".format(chance,round(sum_prob*100,4),"%")
-    plt.text(0,.3,text)
-    # Show the plot
+    # Plot text in upper left of graph, showing correlation
+    text = "P of making at least {} shots: {}{}".format(r,round(sum_prob*100,4),"%")
+    ax.text(0.3,.9,text, ha='center',va='center',transform=ax.transAxes)
+
+    # Show the plot and label axis
+    plt.xlabel("Shots Total")
+    plt.ylabel("Probability")
+    plt.axvline(x=r, c='r')
     plt.show()
 # Call our function to return the probability that out of 10 shots, at 85% per shot, Mr. Cole will
 # make 5 shots
+
+# Function call works like this: plot_prob(n, %chance per event, r)
 plot_prob(10,.9,8)
